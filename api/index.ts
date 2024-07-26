b3d1e5bd19432405c9c164fb845aae3cb0257ce6
Aissamdev2 <shiranui201103@gmail.com>
2024-07-26 18:17:47 +0200

    cors

--- api/index.ts
+++ api/index.ts
@@ -14,7 +14,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
